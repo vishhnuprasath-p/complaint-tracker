6a0b40078d114113aeb68f8ab5154c602f37d556
vijayrajrangan <vijayrajrangan@gmail.com>
2026-04-21 11:17:02 +0530

    fix port

--- app.js
+++ app.js
@@ -9,4 +9,5 @@ app.use(express.static('public'));
 const complaintsRouter = require('./routes/complaints');
 app.use('/api/complaints', complaintsRouter);
 
-app.listen(3000, () => console.log('Server running on http://localhost:3000'));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log('Server running'));
